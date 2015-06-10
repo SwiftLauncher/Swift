ac8f55baca831743343eda72b055e2eec53816cb
Tim Vinkemeier <timvinkemeier@live.de>
2015-06-11 00:06:17 +0200

    Removed unnecessary methods.

--- Swift.Extensibility.Internal/IPluginManager.cs
+++ Swift.Extensibility.Internal/IPluginManager.cs
@@ -1,32 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.Composition;
 using Swift.Extensibility.UI;
 
-namespace Swift.Extensibility
+namespace Swift.Extensibility.Internal
 {
-    [InheritedExport]
     public interface IPluginManager
     {
         IList<MenuItem> MenuItems { get; }
-
-        /// <summary>
-        /// Initializes the plugins.
-        /// </summary>
-        void InitializePlugins();
-
-        /// <summary>
-        /// Distributes the logging channels.
-        /// </summary>
-        void DistributeLoggingChannels();
-
-        /// <summary>
-        /// Distributes the plugin services to all plugin service users.
-        /// </summary>
-        void DistributePluginServices();
-
-        /// <summary>
-        /// Shuts down the plugins.
-        /// </summary>
-        void ShutdownPlugins();
     }
 }
